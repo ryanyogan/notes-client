648670508bd5953afc5aaef3f06f0af5507de2ee
Ryan Yogan <ryanyogan@gmail.com>
2018-10-24 12:45:29 -0700

    Added in a basic stub

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+import Routes from "./Routes";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">Notes</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <LinkContainer to="/signup">
+                <NavItem href="/signup">Signup</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <NavItem href="/login">Login</NavItem>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes />
       </div>
     );
   }
---
+++ src/containers/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Notes</h1>
+          <p>A Serverless example using Amplify</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
